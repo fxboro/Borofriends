a3709c4893e5e67e4d313ebc3ac8bfb3a2b13589
Chima Ohiagbaji <chima.ohiagbaji@denkwerk.com>
2019-09-27 16:46:13 +0200

    modified Cardd.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email}) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src='https://robohash.org/borobo?200x200' />  
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src='https://robohash.org/borobo?100x100' />  
           <div>
-            <h2>John Donavan</h2>
-            <p>john.donavan@gmail.com</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
             </div>
         </div>
     );
