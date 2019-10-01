dd0625ee7957e933b451c1c08e97fd3f724fabfc
Chima Ohiagbaji <chima.ohiagbaji@denkwerk.com>
2019-10-01 17:05:21 +0200

    modified cardList

--- src/CardList.js
+++ src/CardList.js
@@ -3,14 +3,21 @@ import Card from './Card';
 
 
 const CardList = ({ robots}) => {
+    const cardComponent = robots.map((user, i) => {
+        return (
+        <Card 
+            key={i} i
+            d={robots[i].id} 
+            name={robots[i].name} 
+            email={robots[i].email} 
+        />
+        );
+    })
     return (
         <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-        <Card id={robots[3].id} name={robots[2].name} email={robots[3].email} />
+        { cardComponent }
         </div>
     );
-
 }
 
 export default CardList;
